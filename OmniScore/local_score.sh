00cff8c96bfc24c55758865e66cad73eb59339f4
haoyu-eez <2928826539@qq.com>
2025-05-31 19:06:16 +0800

    refacotr

--- OmniScore/local_score.sh
+++ OmniScore/local_score.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 # === Configuration ===
 # wget https://huggingface.co/datasets/Haoyuwu/VideoDPODataset/resolve/main/vidpro10k-short-cogvideo.tar?download=true  
 # tar -xvf vidpro10k-short-cogvideo.tar
 video_dir="vidpro10k-short-cogvideo"
 eval_dir="vidpro10k-short-cogvideo/scores"
-video_dir="vidpro-test"
-eval_dir="vidpro-test/scores"
+# we provide a mini dataset for test code 
+# video_dir="vidpro-test"
+# eval_dir="vidpro-test/scores"
 DIMENSION="dynamic_degree motion_smoothness subject_consistency overall_consistency aesthetic_quality imaging_quality temporal_flickering"
 start_idx=0
 PROMPT_FILE=${PROMPT_FILE:-""}  # 允许外部设置 PROMPT_FILE，默认为空
-PROMPT_FILE="/home/haoyu/research/vidpro10k-1-list.json"
+PROMPT_FILE="../prompts/vidpro10k-1-dict.json"
 # === Function to process one subfolder ===
 process_subfolder() {
     local subfolder=$1
@@ -39,7 +40,6 @@ process_subfolder() {
         python compute_total_score.py --score_dir "$output_path"
 
         echo "[Done] Completed subfolder $subfolder."
-        exit 
     else
         echo "[Skip] Subfolder $subfolder already processed, skipping..."
     fi
